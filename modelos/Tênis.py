4170bc088e32a9d624fbbeba7a445eb0990d8cdd
lele <140726302+leoda17@users.noreply.github.com>
2024-09-05 12:10:33 -0300

    Add files via upload

---
+++ modelos/Tênis.py
@@ -0,0 +1,10 @@
+class Tênis:
+        nome = ''  
+        modelo = '' 
+        Local = ''
+        ativo = False 
+
+Tênis_nikerenewelavate3 = Tênis()
+Tênis_LeBronwitenness8 = Tênis()
+
+Tênis = (Tênis_nikerenewelavate3, Tênis_LeBronwitenness8)
